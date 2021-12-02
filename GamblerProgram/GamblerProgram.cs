d14b37e7c8991461d93b038c982e09ebc82019a6
Shrimaj <shrimaj.mehta60@gmail.com>
2021-12-02 19:08:15 +0530

    Add project files.

---
+++ GamblerProgram/GamblerProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerProgram
+{
+    public class GamblerProgram
+    {
+        public int stake = 100;
+
+        public int bet = 1;
+    }
+}
